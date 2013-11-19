87ab30f869531a528c786e43429c91673ec39000
L Lumbreras <llumbreras@gmail.com>
2013-11-18 19:10:50 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheerswithpeers::Application.config.secret_token = '0e024b5791e9dfe0ebef2ac15c338a655276ef4717feab389c1cde75efec4539b88a1268a8ee371c0498aac991a78f19cafe2897ab2084bcb43fcec542afa116'
